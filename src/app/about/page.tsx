ae5bbbd8d1bb4aa54c962a2b7d21bade68f5545c
Lamin Jawla <laminjawla1001@gmail.com>
2024-03-12 16:16:30 +0000

    About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,69 @@
+import Navbar from '@/components/navbar/Navbar';
+import React from 'react';
+import Link from 'next/link';
+import TeamComponent from "@/components/team/team"
+
 export default function About() {
-    return (
-        <div className="h-screen">
-            About
+  return (
+    <>
+      <Navbar/>
+      <div
+        className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center"
+        style={{
+          backgroundImage: `url(/images/hero3.jpeg)`,
+          minHeight: "100vh",
+        }}
+      >
+        <div
+          className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}
+        >
+          <div className="flex h-full items-center justify-center">
+            <div className="text-white">
+              <h1 className="mb-4 text-4xl font-semibold">Story Forgers</h1>
+              <h5 className="mb-6 text-xl font-semibld">A place to forge a tale and inspire millions.</h5>
+              <Link
+                href="/forge"
+                type="button"
+                className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                data-twe-ripple-init
+                data-twe-ripple-color="light"
+              >
+                Get Started
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 m-auto p-5 items-center">
+      {/* Left side with text */}
+      <div className="items-center p-4">
+        <h1 className="text-4xl font-bold mb-4 text-center">What it is?</h1>
+        <p className='text-center items-center justify-center'>"Story Forgers was born from the desire to cultivate a vibrant community of passionate storytellers. It serves as a hub for connecting individuals who share a love for crafting compelling narratives, fostering an environment where incredible stories are woven, and where inspiration has the power to transform lives."</p>
+      </div>
+
+      {/* Right side with video */}
+      <div className="relative border rounded-md overflow-hidden">
+        <video style={{ width: '100%' }} autoPlay loop muted className="w-full h-full object-cover">
+          <source src="/gifs/storyforgers.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+    <TeamComponent/>
+
+
+    <div className='flex items-center text-center justify-center py-10'>
+        <Link
+            target="_blank"
+            href="https://github.com/RadouaneBaba/StoryForgers"
+            type="button"
+            className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+            data-twe-ripple-init
+            data-twe-ripple-color="light">
+            Visit The Repository
+        </Link>
+    </div>
+    </>
+  );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export default async function Navbar() {
   const user = await getUser();
 
   return (
-    <nav className="p-5 shadow bg-slate-900 md:flex md:items-center md:justify-between fixed">
+    <nav className="p-5 shadow bg-slate-900 md:flex md:items-center md:justify-between">
       {/* Logo and project name */}
       <div className="flex justify-between items-center mx-2">
         <div className="text-2xl pl-5 md:pl-0 font-semibold text-slate-50l cursor-pointer">
@@ -19,7 +19,7 @@ export default async function Navbar() {
       </div>
 
       {/* Nav items and links */}
-      <ul className="md:flex md:items-center z-[-1] md:z-auto md:static left-0 md:w-auto w-full md:py-0 py-4 md:pl-0 pl-5 top-[-400px] transition-all ease-in duration-500">
+      <ul className="md:flex md:items-center md:z-auto md:static left-0 md:w-auto w-full md:py-0 py-4 md:pl-0 pl-5 transition-all ease-in duration-500">
         <li className="mx-4 my-6 md:my-0">
           <Link href="#" className="hover:text-slate-100 text-xl duration-500">Home</Link>
         </li>
@@ -29,17 +29,20 @@ export default async function Navbar() {
         <li className="mx-4 my-6 md:my-0">
           <Link href="#showcase" className="hover:text-slate-100 text-xl duration-500">Showcase</Link>
         </li>
+        <li className="mx-4 my-6 md:my-0">
+          <Link href="/about" className="hover:text-slate-100 text-xl duration-500">About</Link>
+        </li>
       </ul>
 
       {/* Authentication */}
       {
         user ? (
-          <div className="flex items-center md:pl-0 pl-5 top-[-400px] transition-all ease-in duration-500">
+          <div className="flex items-center md:pl-0 pl-5 transition-all ease-in duration-500">
             <div>{user.given_name}</div>
             <LogoutLink className="text-slate-900 bg-slate-100 hover:bg-slate-50 rounded-md p-2 mx-6 font-bold">Logout</LogoutLink>
           </div>
         ) : (
-            <div className="md:pl-0 pl-5 top-[-400px] transition-all ease-in duration-500">
+            <div className="md:pl-0 pl-5 transition-all ease-in duration-500">
               <LoginLink className="text-slate-900 bg-slate-200 hover:bg-slate-50 font-bold rounded-md font-[Poppins] duration-500 px-6 py-2 mx-4">Sign in</LoginLink>
               <RegisterLink className="hover:text-slate-100">Sign up</RegisterLink>
             </div>
